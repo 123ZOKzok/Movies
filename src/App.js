358c6f28d2d985e84965e03e60776d9b8b8cb6a1
123ZOKzok <user email>
2022-10-03 16:43:58 +0300

    Add a function

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import { useEffect } from "react";
 const API_URL='http://www.omdbapi.com/?apikey=c5871e50';
 
 const App =() => {
- 
+  const searchMovies= async ()
+
+ useEffect=(()=>{
+
+ }, []);
   return (
     <div className="App">
      
