b0ede1220a14eb743a73197174b7cd8ad50fc933
123ZOKzok <user email>
2022-10-03 17:20:28 +0300

    search bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import { useEffect } from "react";
-const API_URL='http://www.omdbapi.com/?apikey=c5871e50';
+import './App.css';
+
+const API_URL = 'http://www.omdbapi.com/?apikey=c5871e50';
 
 const App =() => {
   const searchMovies= async (title)=> {
     const response= await fetch(`${API_URL}&s={title}`);
+    const data= await response.json();
+
+    console.log(data.Search);
   }
 
  useEffect=(()=>{
-
+searchMovies(`Spiderman`);
  }, []);
   return (
     <div className="App">
+     <h1>Movieland</h1>
+     <div className="search"> 
+     <input
+     placeholder="search for movies"
+     value="Sistas"
      
-    
-     
+     />
+     </div>
     </div>
   );
 }
