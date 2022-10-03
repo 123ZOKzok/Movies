96b39543d2924687fdab55331bb2ba7f2d94add8
123ZOKzok <user email>
2022-10-03 16:49:32 +0300

    fetch API

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { useEffect } from "react";
 const API_URL='http://www.omdbapi.com/?apikey=c5871e50';
 
 const App =() => {
-  const searchMovies= async ()
+  const searchMovies= async (title)=> {
+    const response= await fetch(`${API_URL}&s={title}`);
+  }
 
  useEffect=(()=>{
 
